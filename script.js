b3ead654fc0ba4596b5aa9207c1f74501d964243
ghghgh333 <ghirano@keio.jp>
2025-06-11 12:10:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // アニメーションさせたい要素を選択
-    const content = document.querySelector('.fade-in');
 
-    // ページが読み込まれたら 'is-visible' クラスを追加して表示させる
-    if (content) {
-        content.classList.add('is-visible');
-    }
-});
